d44912c08cd55aea8bd2e7c6230d41d4c18efc44
Tom Meyrick <tomgmeyrick@gmail.com>
2020-09-14 19:11:47 +0100

    Added repeating functions to Accordian

--- src/Components/accordian.js
+++ src/Components/accordian.js
@@ -2,60 +2,8 @@ import React, { Component } from "react";
 import Accordion from "react-bootstrap/Accordion";
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
-import single from "../single.json";
 import multiple from "../multiple.json";
 import axios from "../axiosConfig";
-
-// class Accordian extends Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       loaded: false,
-//       data: [],
-//     };
-//   }
-
-//   componentDidMount() {
-//     axios.get(`/${this.id}/data`).then(({ data }) => {
-//       this.setState({
-//         loaded: true,
-//         data: data.data,
-//       });
-//     });
-//   }
-
-//   render() {
-//     let { loaded, data } = this.state;
-//     return (
-//       <div className="container">
-//         <Accordion defaultActiveKey="0">
-//           <Card key={single.id}>
-//             <Card.Header>
-//               <Accordion.Toggle as={Button} variant="link" eventKey="0">
-//                 {single.name}
-//               </Accordion.Toggle>
-//             </Card.Header>
-//             <Accordion.Collapse eventKey="0">
-//               <Card.Body>
-//                 <div className="row">
-//                   <p className="col">{single.rating}</p>
-//                   <p className="col">{single.priciness}</p>
-//                 </div>
-//                 <img
-//                   className="rounded-circle"
-//                   src="../assets/van01.png"
-//                   alt="An ice cream man"
-//                 ></img>
-//                 <h5>Bio</h5>
-//                 <p>{single.bio}</p>
-//               </Card.Body>
-//             </Accordion.Collapse>
-//           </Card>
-//         </Accordion>
-//       </div>
-//     );
-//   }
-// }
 
 class Accordian extends Component {
   constructor(props) {
@@ -75,8 +23,24 @@ class Accordian extends Component {
     });
   }
 
+  repeatStars(value) {
+    const stars = [];
+    for (let i = 0; i < value; i += 1) {
+      stars.push(<img src="../assets/star-full.png" alt="stars"></img>);
+    }
+    return stars;
+  }
+
+  repeatPounds(value) {
+    const pounds = [];
+    for (let i = 0; i < value; i += 1) {
+      pounds.push(<img src="../assets/star-empty.png" alt="pounds"></img>);
+    }
+    return pounds;
+  }
+
   render() {
-    let { loaded, vendors } = this.state;
+    // let { loaded, vendors } = this.state;
     return (
       <div className="container">
         <Accordion defaultActiveKey="0">
@@ -89,13 +53,13 @@ class Accordian extends Component {
               </Card.Header>
               <Accordion.Collapse eventKey={index}>
                 <Card.Body>
+                  <div className="row">{this.repeatStars(vendor.rating)}</div>
                   <div className="row">
-                    <p className="col">{vendor.rating}</p>
-                    <p className="col">{vendor.priciness}</p>
+                    {this.repeatPounds(vendor.priciness)}
                   </div>
                   <img
                     className="rounded-circle"
-                    src={vendor.imgUrl}
+                    src={"../assets/van01.png"}
                     alt="An ice cream man"
                   ></img>
                   <h5>Bio</h5>
