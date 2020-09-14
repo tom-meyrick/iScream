3ef01a52a6675fdb132890f1b37c11596e573efa
Tom Meyrick <tomgmeyrick@gmail.com>
2020-09-14 15:29:17 +0100

    Created Axios request for accordian

--- src/Components/accordian.js
+++ src/Components/accordian.js
@@ -3,9 +3,28 @@ import Accordion from "react-bootstrap/Accordion";
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
 import single from "../single.json";
+import axios from "../axiosConfig";
 
 class Accordian extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loaded: false,
+      data: [],
+    };
+  }
+
+  componentDidMount() {
+    axios.get(`/${this.id}/data`).then(({ data }) => {
+      this.setState({
+        loaded: true,
+        data: data.data,
+      });
+    });
+  }
+
   render() {
+    let { loaded, data } = this.state;
     return (
       <div className="container">
         <Accordion defaultActiveKey="0">
@@ -21,7 +40,11 @@ class Accordian extends Component {
                   <p className="col">{single.rating}</p>
                   <p className="col">{single.priciness}</p>
                 </div>
-                <img className="rounded-circle" src={single.imgUrl}></img>
+                <img
+                  className="rounded-circle"
+                  src={single.imgUrl}
+                  alt="An ice cream man"
+                ></img>
                 <h5>Bio</h5>
                 <p>{single.bio}</p>
               </Card.Body>
@@ -32,5 +55,62 @@ class Accordian extends Component {
     );
   }
 }
+
+//Completed API component
+
+// class Accordian extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       loaded: false,
+//       vendors: [],
+//     };
+//   }
+
+//   componentDidMount() {
+//     axios.get(`/${this.id}/data`).then(({ data }) => {
+//       this.setState({
+//         loaded: true,
+//         vendors: data.data,
+//       });
+//     });
+//   }
+
+//   render() {
+//     let { loaded, data } = this.state;
+//     return !loaded ? (
+//       <p>Loading...</p>
+//     ) : (
+//       <div className="container">
+//         {data.map((vendor) => (
+//           <Accordion defaultActiveKey="0">
+//             <Card key={vendor.id}>
+//               <Card.Header>
+//                 <Accordion.Toggle as={Button} variant="link" eventKey="0">
+//                   {vendor.name}
+//                 </Accordion.Toggle>
+//               </Card.Header>
+//               <Accordion.Collapse eventKey="0">
+//                 <Card.Body>
+//                   <div className="row">
+//                     <p className="col">{vendor.rating}</p>
+//                     <p className="col">{vendor.priciness}</p>
+//                   </div>
+//                   <img
+//                     className="rounded-circle"
+//                     src={vendor.imgUrl}
+//                     alt="An ice cream man"
+//                   ></img>
+//                   <h5>Bio</h5>
+//                   <p>{vendor.bio}</p>
+//                 </Card.Body>
+//               </Accordion.Collapse>
+//             </Card>
+//           </Accordion>
+//         ))}
+//       </div>
+//     );
+//   }
+// }
 
 export default Accordian;
