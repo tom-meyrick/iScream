8fc2032b6129246df2066b63c1a6bf31bd454322
Tom Meyrick <tomgmeyrick@gmail.com>
2020-09-14 10:57:44 +0100

    Created basic structure and imported into app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
 import React from "react";
+import Heading from "./Components/heading";
+import leafletMap from "./Components/leafletMap";
+import Accordian from "./Components/accordian";
 
-function App() {}
+function App() {
+  <>
+    <Heading />
+    <leafletMap />
+    <Accordian />
+  </>;
+}
 
 export default App;
