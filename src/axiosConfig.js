9437e384e0c66e2036426d15f9bc096002b77fe1
Tom Meyrick <tomgmeyrick@gmail.com>
2020-09-14 14:09:47 +0100

    Fixed naming error

---
+++ src/axiosConfig.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+export default axios.create({
+  baseURL: "https://iscream/api/",
+  headers: {
+    Accept: "application/json",
+
+    // Authorization:
+    //   "Bearer JX8IgQYjo2lIZ8B0OUPHiVsbQUNRGkJiKQXeiVACUUJCdjSp0FJUM4gxAKBxQQ1aeN4DPMtdJUoBLIDG",
+  },
+});
