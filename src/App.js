31b30c491cea2ed8d9eb6b5783fb823f99a18a00
Tom Meyrick <tomgmeyrick@gmail.com>
2020-09-14 12:10:52 +0100

    Added div for map

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import SideBar from "./Components/SideBar";
 const App = () => (
   <>
     <div className="container">
-      <h1>Project Library</h1>
+      <h1>Pattern Library</h1>
       <hr></hr>
       <h5>Title</h5>
       <Heading />
@@ -15,6 +15,8 @@ const App = () => (
       <Accordian />
       <h5>SideBar</h5>
       <SideBar />
+      <h5>Map</h5>
+      <LeafletMap />
     </div>
   </>
 );
---
+++ src/Components/leafletMap.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class LeafletMap extends Component {
+  render() {
+    return <div className="leafletMap" id="leafletMap"></div>;
+  }
+}
+
+export default LeafletMap;
