67857534691cb91b1322903d42d1339618e67317
Tom Meyrick <tomgmeyrick@gmail.com>
2020-09-14 19:22:20 +0100

    Reduced to a single repeating function

--- src/Components/accordian.js
+++ src/Components/accordian.js
@@ -2,15 +2,16 @@ import React, { Component } from "react";
 import Accordion from "react-bootstrap/Accordion";
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
-import single from "../single.json";
+import multiple from "../multiple.json";
 import axios from "../axiosConfig";
+import Profile from "./Profile";
 
 class Accordian extends Component {
   constructor(props) {
     super(props);
     this.state = {
       loaded: false,
-      data: [],
+      vendors: [],
     };
   }
 
@@ -18,99 +19,67 @@ class Accordian extends Component {
     axios.get(`/${this.id}/data`).then(({ data }) => {
       this.setState({
         loaded: true,
-        data: data.data,
+        vendors: data.data,
       });
     });
   }
 
+  repeatElements(value, src) {
+    const elements = [];
+    for (let i = 0; i < value; i += 1) {
+      elements.push(<img src={src} alt="stars"></img>);
+    }
+    return elements;
+  }
+
+  styleTab = (i) => {
+    const styles = ["pink", "caramel", "chocolate", "blue", "vanilla"];
+    return styles[i % styles.length];
+  };
+  styleBody = (i) => {
+    const styles = ["blue", "vanilla", "pink", "vanilla", "chocolate"];
+    return styles[i % styles.length];
+  };
+  styleProfile = (i) => {
+    const styles = ["caramel", "chocolate", "blue", "pink", "vanilla"];
+    return styles[i % styles.length];
+  };
+
   render() {
-    let { loaded, data } = this.state;
+    // let { loaded, vendors } = this.state;
     return (
       <div className="container">
         <Accordion defaultActiveKey="0">
-          <Card key={single.id}>
-            <Card.Header>
-              <Accordion.Toggle as={Button} variant="link" eventKey="0">
-                {single.name}
-              </Accordion.Toggle>
-            </Card.Header>
-            <Accordion.Collapse eventKey="0">
-              <Card.Body>
-                <div className="row">
-                  <p className="col">{single.rating}</p>
-                  <p className="col">{single.priciness}</p>
-                </div>
-                <img
-                  className="rounded-circle"
-                  src={single.imgUrl}
-                  alt="An ice cream man"
-                ></img>
-                <h5>Bio</h5>
-                <p>{single.bio}</p>
-              </Card.Body>
-            </Accordion.Collapse>
-          </Card>
+          {multiple.map((vendor, index) => (
+            <Card key={vendor.id}>
+              <Card.Header className={`accord-${this.styleTab(index)}`}>
+                <Accordion.Toggle as={Button} variant="link" eventKey={index}>
+                  <div className="accord-title">{vendor.name}</div>
+                </Accordion.Toggle>
+              </Card.Header>
+              <Accordion.Collapse eventKey={index}>
+                <Card.Body>
+                  <div className="row">
+                    {this.repeatElements(vendor.rating, "asset")}
+                  </div>
+                  <div className="row">
+                    {this.repeatElements(vendor.priciness, "asset")}
+                  </div>
+                  <img
+                    className="rounded-circle"
+                    src={"../assets/van01.png"}
+                    alt="An ice cream man"
+                  ></img>
+                  <h5>Bio</h5>
+                  <p>{vendor.bio}</p>
+                </Card.Body>
+              </Accordion.Collapse>
+            </Card>
+          ))}
         </Accordion>
       </div>
     );
   }
 }
-
-//Completed API component
-
-// class Accordian extends Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       loaded: false,
-//       vendors: [],
-//     };
-//   }
-
-//   componentDidMount() {
-//     axios.get(`/${this.id}/data`).then(({ data }) => {
-//       this.setState({
-//         loaded: true,
-//         vendors: data.data,
-//       });
-//     });
-//   }
-
-//   render() {
-//     let { loaded, data } = this.state;
-//     return !loaded ? (
-//       <p>Loading...</p>
-//     ) : (
-//       <div className="container">
-//         {data.map((vendor) => (
-//           <Accordion defaultActiveKey="0">
-//             <Card key={vendor.id}>
-//               <Card.Header>
-//                 <Accordion.Toggle as={Button} variant="link" eventKey="0">
-//                   {vendor.name}
-//                 </Accordion.Toggle>
-//               </Card.Header>
-//               <Accordion.Collapse eventKey="0">
-//                 <Card.Body>
-//                   <div className="row">
-//                     <p className="col">{vendor.rating}</p>
-//                     <p className="col">{vendor.priciness}</p>
-//                   </div>
-//                   <img
-//                     className="rounded-circle"
-//                     src={vendor.imgUrl}
-//                     alt="An ice cream man"
-//                   ></img>
-//                   <h5>Bio</h5>
-//                   <p>{vendor.bio}</p>
-//                 </Card.Body>
-//               </Accordion.Collapse>
-//             </Card>
-//           </Accordion>
-//         ))}
-//       </div>
-//     );
-//   }
-// }
 
 export default Accordian;
