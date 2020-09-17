593bf48994ba88e09cb1f12595359c2592cdf9f0
Will <will_leighton@outlook.com>
2020-09-17 15:40:46 +0100

    Updates

---
+++ precache-manifest.7f973fbb1b696924f7b8ba243787d212.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b8997aa8f2969afeffba866482c50d1",
+    "url": "/iScream/index.html"
+  },
+  {
+    "revision": "237204fec413bbfd07a3",
+    "url": "/iScream/static/css/2.a4567992.chunk.css"
+  },
+  {
+    "revision": "09604c21698dc0ce41c9",
+    "url": "/iScream/static/css/main.f5b30c3d.chunk.css"
+  },
+  {
+    "revision": "237204fec413bbfd07a3",
+    "url": "/iScream/static/js/2.83e5321b.chunk.js"
+  },
+  {
+    "revision": "17dffa516c2ca25cecdb67ecbc5e1c6e",
+    "url": "/iScream/static/js/2.83e5321b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09604c21698dc0ce41c9",
+    "url": "/iScream/static/js/main.7c8185ef.chunk.js"
+  },
+  {
+    "revision": "7519005ff9aa56acf14d",
+    "url": "/iScream/static/js/runtime-main.01a0a665.js"
+  },
+  {
+    "revision": "d850698ab4cb56b9b5ee61974ac1d19c",
+    "url": "/iScream/static/media/sprinkles-texture02.d850698a.jpg"
+  },
+  {
+    "revision": "c063bd8097149973b4f0c80aeff2a469",
+    "url": "/iScream/static/media/waffle-texture.c063bd80.jpg"
+  }
+]);
