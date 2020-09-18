d13511e05ecdd715fb5ab8af2d32004cc18815a6
Will <will_leighton@outlook.com>
2020-09-18 12:58:22 +0100

    font fixes

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -3,8 +3,8 @@ import React from "react";
 export default function Footer({ className }) {
   return (
     <p className={"footer__text"}>
-      developed by Team Hopper, this was a group project for cohort #17 of
-      developMe in Bristol.
+      bckend API by Gilson Almeida Leaflet and Styling by Will Leighton and
+      React Redux by Tom Meyrick. #cohort-17
     </p>
   );
 }
